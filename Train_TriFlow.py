615ab11481c2fe5e60f040ff5dab86d65dbcc164
Omid <omid.mirzaei@gmail.com>
2019-11-07 23:01:08 -0500

    TriFlow training

--- Train_TriFlow.py
+++ Train_TriFlow.py
@@ -75,23 +75,26 @@ else:
     # Directory of probability and weight tables
     Output_Dir = arguments[arguments.index('-o') + 1]
 
+if os.path.basename(Output_Dir) not in os.listdir(os.path.curdir):
+    os.mkdir(Output_Dir)
+
 # Home directory
 Home_Dir = os.path.curdir
 # Directory of benign apps (.apk files)
 BenignApps_Dir = Input_Dir + '/Benign_Apks'
-# Directory of malwares (.apk files)
+# Directory of malware (.apk files)
 Malwares_Dir = Input_Dir + '/Malware_Apks'
 # Directory of flows extracted from benign apps (.txt files)
 BenignFlows_Dir = Input_Dir + '/Benign_Flows'
-# Directory of flows extracted from malwares (.txt files)
+# Directory of flows extracted from malware (.txt files)
 MalwareFlows_Dir = Input_Dir + '/Malware_Flows'
 
 # ********************* End of Initialization *********************
 
 # ********************* Main Body *********************
 
-print 'Training process started...'
-# Merges benign apps and malwares
+print('Training process started...')
+# Merges benign apps and malware
 if not os.path.exists(os.path.join(Input_Dir,'All_Apks')) or len(os.listdir(os.path.join(Input_Dir,'All_Apks'))) == 0:
     os.mkdir(os.path.join(Input_Dir,'All_Apks'))
     for app in os.listdir(BenignApps_Dir):
@@ -99,7 +102,7 @@ if not os.path.exists(os.path.join(Input_Dir,'All_Apks')) or len(os.listdir(os.p
     for app in os.listdir(Malwares_Dir):
         shutil.copy(os.path.join(Malwares_Dir,app),os.path.join(Input_Dir,'All_Apks',app))
 
-# Merges benign flows and the ones from malwares
+# Merges benign flows and the ones from malware
 if not os.path.exists(os.path.join(Input_Dir,'All_Flows')) or len(os.listdir(os.path.join(Input_Dir,'All_Flows'))) == 0:
     os.mkdir(os.path.join(Input_Dir,'All_Flows'))
     for flow in os.listdir(BenignFlows_Dir):
@@ -108,43 +111,47 @@ if not os.path.exists(os.path.join(Input_Dir,'All_Flows')) or len(os.listdir(os.
         shutil.copy(os.path.join(MalwareFlows_Dir,flow),os.path.join(Input_Dir,'All_Flows',flow))
 
 # Extracts real information flows from FlowDroid output files
-print 'Extracting real information flows from FlowDroid output files...'
+print('Extracting real information flows from FlowDroid output files...')
 subprocess.call([sys.executable, os.path.join(Home_Dir,'Extract_Real_IFs.py'), '-i', os.path.join(Input_Dir,'All_Flows'), '-o', os.path.join(Home_Dir,'Real_Flows')])
 # Extracts total information flows
-print 'Extracting total information flows from applications...'
+print('Extracting total information flows from applications...')
 subprocess.call([sys.executable, os.path.join(Home_Dir,'Extract_Total_IFs.py'), '-i', os.path.join(Input_Dir,'All_Apks'), '-o', os.path.join(Home_Dir,'Total_Flows')])
 # Calculates the probabilities of information flows
-print 'Calculating the probabilities of information flows...'
+print('Calculating the probabilities of information flows...')
 subprocess.call([sys.executable, os.path.join(Home_Dir,'Probability.py'), '-r', os.path.join(Home_Dir,'Real_Flows'), '-t', os.path.join(Home_Dir,'Total_Flows/Flows'), '-o', Output_Dir])
-print 'Probability table was created...'
+print('Probability table was created...')
 
-# Separating real information flows of malwares and benign applications
-print 'Separating real information flows of malwares and benign applications...'
-os.mkdir(os.path.join(Home_Dir,'Real_Flows_Benign'))
+# Separating real information flows of malware and benign applications
+print('Separating real information flows of malware and benign applications...')
+Real_Flows_Benign = os.path.join(Home_Dir,'Real_Flows_Benign')
+if os.path.basename(Real_Flows_Benign) not in os.listdir(os.path.curdir):
+    os.mkdir(Real_Flows_Benign)
 for app in os.listdir(BenignApps_Dir):
     if '.apk' in app:
-        shutil.copy(os.path.join(Home_Dir,'Real_Flows',app + '-realflows.txt'),os.path.join(Home_Dir,'Real_Flows_Benign',app + '-realflows.txt'))
-os.mkdir(os.path.join(Home_Dir,'Real_Flows_Malware'))
+        shutil.copy(os.path.join(Home_Dir,'Real_Flows',app + '-realflows.txt'),os.path.join(Real_Flows_Benign, app + '-realflows.txt'))
+Real_Flows_Malware = os.path.join(Home_Dir,'Real_Flows_Malware')
+if os.path.basename(Real_Flows_Malware) not in os.listdir(os.path.curdir):
+    os.mkdir(Real_Flows_Malware)
 for app in os.listdir(Malwares_Dir):
     if '.apk' in app:
-        shutil.copy(os.path.join(Home_Dir,'Real_Flows',app + '-realflows.txt'),os.path.join(Home_Dir,'Real_Flows_Malware',app + '-realflows.txt'))
+        shutil.copy(os.path.join(Home_Dir,'Real_Flows',app + '-realflows.txt'),os.path.join(Real_Flows_Malware, app + '-realflows.txt'))
 
 # Calculates the frequencies of information flows in benign apps
-print 'Calculating the frequencies of information flows in benign applications...'
-subprocess.call([sys.executable, os.path.join(Home_Dir,'Frequency.py'), '-i', os.path.join(Home_Dir,'Real_Flows_Benign'), '-o', Output_Dir])
+print('Calculating the frequencies of information flows in benign applications...')
+subprocess.call([sys.executable, os.path.join(Home_Dir,'Frequency.py'), '-i', Real_Flows_Benign, '-o', Output_Dir])
 os.rename(os.path.join(Output_Dir,'Freq_InfoFlows_Sorted.csv'),os.path.join(Output_Dir,'Freq_InfoFlows_Benign.csv'))
-print 'Frequency table was created for benign applications...'
-# Calculates the frequencies of information flows in malwares
-print 'Calculating the frequencies of information flows in malwares...'
-subprocess.call([sys.executable, os.path.join(Home_Dir,'Frequency.py'), '-i', os.path.join(Home_Dir,'Real_Flows_Malware'), '-o', Output_Dir])
+print('Frequency table was created for benign applications...')
+# Calculates the frequencies of information flows in malware
+print('Calculating the frequencies of information flows in malicious applications...')
+subprocess.call([sys.executable, os.path.join(Home_Dir,'Frequency.py'), '-i', Real_Flows_Malware, '-o', Output_Dir])
 os.rename(os.path.join(Output_Dir,'Freq_InfoFlows_Sorted.csv'),os.path.join(Output_Dir,'Freq_InfoFlows_Malware.csv'))
-print 'Frequency table was created for malwares...'
+print('Frequency table was created for malicious applications...')
 # Calculates the weights of information flows
-print 'Calculating the weights of information flows...'
+print('Calculating the weights of information flows...')
 subprocess.call([sys.executable, os.path.join(Home_Dir,'Weighting.py'), '-i', Output_Dir, '-o', Output_Dir])
-print 'Weight table was created...'
+print('Weight table was created...')
 
-print 'End of Training!'
+print('End of Training!')
 
 # ********************* End of Main Body *********************
 
